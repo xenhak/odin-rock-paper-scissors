2f859f494f273837dc1087a7b572d474f8ac166a
Mohsin <154886402+xenhak@users.noreply.github.com>
2024-07-12 02:31:02 +0530

    create a function to generate random choices for computer

---
+++ script.js
@@ -0,0 +1,11 @@
+const getComputerChoice = () => {
+  let randomChoice = Math.floor(Math.random * 3);
+
+  if (randomNumber === 0) {
+    randomChoice = "rock";
+  } else if (randomChoice === 1) {
+    randomChoice = "paper";
+  } else {
+    randomChoice === "scissors";
+  }
+};
