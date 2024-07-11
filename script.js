05709dcca8233ad4bb92c789db968b226b41cff5
Mohsin <154886402+xenhak@users.noreply.github.com>
2024-07-12 02:36:05 +0530

    update getComputerChoice function

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const getComputerChoice = () => {
-  let randomChoice = Math.floor(Math.random * 3);
+  let randomChoice = Math.floor(Math.random() * 3);
 
-  if (randomNumber === 0) {
+  if (randomChoice === 0) {
     randomChoice = "rock";
   } else if (randomChoice === 1) {
     randomChoice = "paper";
   } else {
-    randomChoice === "scissors";
+    randomChoice = "scissors";
   }
+
+  return randomChoice;
 };
+
+// getComputerChoice();
+console.log(getComputerChoice());
